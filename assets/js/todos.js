b9e7bc133b4b0fb3c9b0591fd008074824635c0f
Ethan Palas <ejames711@gmail.com>
2020-04-30 14:40:35 -0500

    add date and remove feature to header

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,30 +1,43 @@
-// Check Off Specific Todos By Clicking
-$("ul").on("click", "li", function(){
-	$(this).toggleClass("completed");
+// cross through todo when completed
+$('body').on('click', 'li', function() {
+	$(this).toggleClass('completed');
 });
 
-//Click on X to delete Todo
-$("ul").on("click", "span", function(event){
-	$(this).parent().fadeOut(500,function(){
-		$(this).remove();
-	});
-	event.stopPropagation();
-});
-
-$("input[type='text']").keypress(function(event){
-	if(event.which === 13){
+//entering new todo on chosen list
+$('body').on('keypress', "input[type='text']", function(event) {
+	if (event.which === 13) {
 		//grabbing new todo text from input
-		var todoText = $(this).val();
-		$(this).val("");
-		var todoDiv = $(this).parent().find("ul");
+		let todoText = $(this).val();
+		$(this).val('');
+		let todoDiv = $(this).parent().find('ul');
 		//create a new li and add to ul
-		todoDiv.append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+		todoDiv.append("<li><span><i class='fa fa-trash'></i></span> " + todoText + '</li>');
 	}
 });
 
-$(".fa-plus").click(function(event){
-	var enterTodo = $(this).parent().parent().find("input");
+//click on trash to delete todo
+$('body').on('click', 'span', function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+});
+
+//click on minus in header of list to delete list
+$('body').on('click', '.fa-minus-square', function() {
+	$(this).closest('div').fadeOut(500, function() {
+		$(this).remove();
+	});
+});
+
+//minimize input area on given to-do list
+$('body').on('click', '.fa-plus', function() {
+	let enterTodo = $(this).parent().parent().find('input');
 	enterTodo.fadeToggle();
 });
 
 //add new todo list
+$('.fa-list').on('click', function(event) {
+	let newList =
+		'<div class="todo"><h1>To-Do List <input type="date"><i class="fa fa-minus-square"></i><i class="fa fa-plus"></i></h1><input type="text" placeholder="Add New Todo"><ul></ul>';
+	$(this).closest('body').append(newList);
+});
